c2ffb2f1952be6a813065dc3472621501e290046
wredwards94 <wredwards94@outlook.com>
2024-04-01 17:08:11 -0400

    add UserPlan and User entities

---
+++ Entities/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class User
+    {
+        Guid Id { get; set; }
+
+        [Required]
+        public string? FirstName { get; set; }
+
+        [Required]
+        public string? LastName { get; set; }
+
+        [Required]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+        public string? Address { get; set; }
+
+        //Collection Property for phone plan
+    }
+}
---
+++ Entities/UserPlan.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities
+{
+    public class UserPlan
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid PlanId { get; set; }
+        public User? User { get; set; }
+        //public PhonePlan? Plan { get; set; }
+
+    }
+}
