2e8a56be7dd5d19320d0f5741c85ae4dccbf963e
daniel <squaredfreezer@gmail.com>
2024-04-01 17:48:06 -0400

    Add UserDevice entity

---
+++ Entities/Device.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities 
+{
+    public class Device 
+    {
+        [Column("DeviceId")]
+        public Guid Id {get; set;}
+        public string? PhoneNumber {get; set;}
+        public string? Manufacturer {get; set;}
+        public string? Model {get; set;}
+    }
+}
