e8e74604d6041ba3d5bfc53958cf0da05d4bdac7
daniel <squaredfreezer@gmail.com>
2024-04-02 17:14:57 -0400

    add Cors & IIS sevice extensions to middleware/pipeline

---
+++ ProjectTwoGroup3/Extensions/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ProjectTwoGroup3.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder
+                    // .WithOrigins("http://www.skillstorm.com")
+                    .AllowAnyOrigin()
+                    // .WithMethods("POST", "PUT", "DELETE", "GET")
+                    .AllowAnyMethod()
+                    // .WithHeaders("accept", "content-type")
+                    .AllowAnyHeader());
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+    }
+}
--- ProjectTwoGroup3/Program.cs
+++ ProjectTwoGroup3/Program.cs
@@ -1,14 +1,37 @@
+using ProjectTwoGroup3.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+} else
+{
+    app.UseHsts();
+}
+
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
