f0eed98d8fcb4b2ab5ccd577133bd7700f97c7d9
daniel <squaredfreezer@gmail.com>
2024-04-04 18:07:59 -0400

    add service manager stuff

---
+++ Contracts/IRepositoryManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IRepositoryManager
+    {
+        IDeviceRepository Device { get; }
+        IPhonePlanRepository PhonePlan { get; }
+        IUserDeviceRepository UserDevice { get; }
+        IUserPlanRepository UserPlan { get; }
+        IUserRepository User { get; }
+        void Save();
+        Task SaveAsync();
+    }
+}
---
+++ Service.Contracts/IServiceManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Contracts
+{
+    public interface IServiceManager
+    {
+        IDeviceService Device { get; }
+        IPhonePlanService PhonePlan { get; }
+        IUserDeviceService UserDevice { get; }
+        IUserService User { get; }
+        IUserPlanService UserPlan { get; }
+    }
+}
