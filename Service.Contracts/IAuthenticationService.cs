0be92339f231a180dc90add54e3ffb61a8d56096
daniel <squaredfreezer@gmail.com>
2024-04-09 13:04:05 -0400

    add AuthenticationService logic, DTOs, and JWT entity

---
+++ Service.Contracts/IAuthenticationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Shared.AuthenticationDtos;
+
+namespace Service.Contracts
+{
+    public interface IAuthenticationService
+    {
+        Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration);
+        Task<bool> ValidateUser (UserForAuthenticationDto userForAuthentication);
+        Task<string> CreateToken();
+    }
+}
---
+++ Service/AuthenticationService.cs
@@ -0,0 +1,108 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities;
+using Entities.ConfigurationModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Shared.AuthenticationDtos;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Service.Contracts;
+
+namespace Service
+{
+    public class AuthenticationService : Contracts.IAuthenticationService
+    {
+
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private readonly IOptions<JwtConfiguration> _configuration;
+        private readonly JwtConfiguration _jwtConfiguration;
+        private User? _user;
+
+        public AuthenticationService(
+            ILoggerManager logger, 
+            IMapper mapper, 
+            UserManager<User> userManager, 
+            IOptions<JwtConfiguration> configuration)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _userManager = userManager;
+            _configuration = configuration;
+            _jwtConfiguration = _configuration.Value;
+        }
+
+        public async Task<string> CreateToken()
+        {
+            var signingCredentials = GetSigningCredentials();
+            var claims = await GetClaims();
+            var tokenOptions = GenerateTokenOptions(signingCredentials, claims);
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+        }
+        public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration)
+        {
+            var user = _mapper.Map<User>(userForRegistration);
+            var result = await _userManager.CreateAsync(user, userForRegistration.Password);
+
+            if(result.Succeeded)
+                await _userManager.AddToRoleAsync(user, userForRegistration.Password);
+            return result;
+        }
+        public async Task<bool> ValidateUser(UserForAuthenticationDto userForAuthentication)
+        {
+            _user = await _userManager.FindByNameAsync(userForAuthentication.Username);
+            var result = _user != null && await _userManager.CheckPasswordAsync(_user, userForAuthentication.Password);
+            if (!result)
+                _logger.LogWarn("Authentication failed. Wrong username or password");
+            return result;
+        }
+        private SigningCredentials GetSigningCredentials()
+        {
+            var key = Encoding.UTF8.GetBytes(_jwtConfiguration.Secret);
+            var secret = new SymmetricSecurityKey(key);
+
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+        private async Task<List<Claim>> GetClaims()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, _user.Username)
+            };
+
+            var roles = await _userManager.GetRolesAsync(_user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            return claims;
+        }
+        private JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials,List<Claim> claims)
+        {
+            var tokenOptions = new JwtSecurityToken
+            (
+                issuer: _jwtConfiguration.ValidIssuer,
+                audience: _jwtConfiguration.ValidAudience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(_jwtConfiguration.Expires)),
+                signingCredentials: signingCredentials
+            );
+
+            return tokenOptions;
+        }
+
+    }
+}
