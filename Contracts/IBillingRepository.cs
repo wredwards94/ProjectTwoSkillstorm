1df2f16f20a6819d97c6505430145818ff58670c
wredwards94 <wredwards94@outlook.com>
2024-04-18 13:05:14 -0400

    refactored getting bills by user id

--- Contracts/IBillingRepository.cs
+++ Contracts/IBillingRepository.cs
@@ -11,7 +11,7 @@ namespace Contracts
     {
         Task<IEnumerable<Billing>> GetAllBillsByUserPlanId(Guid userPlanId, bool trackChanges);
         Task<Billing> GetBillById(Guid billId, bool trackChanges);
-        Task<IEnumerable<Billing>> GetBillsByUserIdPaidFalse(string userId);
+        Task<IEnumerable<Billing>> GetBillsByUserId(string userId, bool trackChanges);
         Task<IEnumerable<Billing>> GetBillsByUserId(string userId);
         void CreateBill(Billing bill);
         void DeleteUserPlanBill(Billing bill);
